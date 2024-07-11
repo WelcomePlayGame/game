6afeb62b259cbb1f9434d8f4d7afd8440c1544ca
welcomepalygame <welcomeplaygame@gmail.com>
2024-07-11 10:22:27 +0300

    11.07.2024_v1

--- components/developer/page-grid-developers.tsx
+++ components/developer/page-grid-developers.tsx
@@ -2,13 +2,20 @@ import DevelopersItem from './page-item-developers';
 
 const DevelopersGrid = ({ developers }: any) => {
   return (
-    <main
-      className={`flex flex-col flex-wrap mt-[30px] lg:mt-[90px] lg:flex-row lg:justify-evenly`}
-    >
-      {developers.map((developer: any, index: number) => (
-        <DevelopersItem key={index} {...developer} />
-      ))}
-    </main>
+    <div>
+      <div
+        className={`flex text-center justify-center mt-[50px] text-[0.8rem]  lg:text-[1.3rem]`}
+      >
+        <h1>Game Developers Hub</h1>
+      </div>
+      <main className="flex flex-col flex-wrap mt-[30px] lg:mt-[90px] lg:flex-row lg:justify-evenly">
+        {developers.map((article: any, index: number) => (
+          <div key={index}>
+            <DevelopersItem {...article} />
+          </div>
+        ))}
+      </main>
+    </div>
   );
 };
 export default DevelopersGrid;
--- components/platform/page-grid - platfrom.tsx
+++ components/platform/page-grid - platfrom.tsx
@@ -2,13 +2,20 @@ import PlatformItem from './page-item-platform';
 
 const PlatformsGrid = ({ platforms }: any) => {
   return (
-    <main
-      className={`flex flex-col flex-wrap mt-[30px] lg:mt-[90px] lg:flex-row lg:justify-evenly`}
-    >
-      {platforms.map((platform: any, index: number) => (
-        <PlatformItem key={index} {...platform} />
-      ))}
-    </main>
+    <div>
+      <div
+        className={`flex text-center justify-center mt-[50px] text-[0.8rem]  lg:text-[1.3rem]`}
+      >
+        <h1>Gaming Platforms Overview</h1>
+      </div>
+      <main className="flex flex-col flex-wrap mt-[30px] lg:mt-[90px] lg:flex-row lg:justify-evenly">
+        {platforms.map((platform: any, index: number) => (
+          <div key={index}>
+            <PlatformItem {...platform} />
+          </div>
+        ))}
+      </main>
+    </div>
   );
 };
 export default PlatformsGrid;
