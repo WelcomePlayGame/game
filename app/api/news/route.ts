dd86f37cad61e453f4b692b5ae63065a01c3e4f2
welcomepalygame <welcomeplaygame@gmail.com>
2024-09-27 16:14:23 +0300

    $(node -p require('./package.json').version)_v2

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,7 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { findAllArticle as get } from '@/lib/action';
 import { NextResponse } from 'next/server';
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+export const GET = async (req: Request) => {
   const news = await get(1, 10);
   return NextResponse.json(news);
 };
