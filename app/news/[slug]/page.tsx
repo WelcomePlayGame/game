87bd5075027ce22abc9892a804f718038b7c3a17
welcomepalygame <welcomeplaygame@gmail.com>
2024-09-28 20:59:01 +0300

    $(node -p 'new Date().toISOString().slice(0, 10)')_v2

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -105,7 +105,7 @@ const Article = async ({ params }: { params: { slug: string } }) => {
           </div>
         </div>
         <div
-          className={`font-normal  pl-[30px] pr-[30px] lg:pt-[70px] lg:pl-[25%] lg:pr-[25%] break-words`}
+          className={`font-normal  pl-[30px] pr-[30px] lg:pt-[70px] lg:pl-[25%] lg:pr-[25%] break-words lg:text-[18px]`}
         >
           <div
             dangerouslySetInnerHTML={createMarkup(article?.content ?? '')}
