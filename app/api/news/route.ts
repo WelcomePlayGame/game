1540a7755e895159a17735d7e8cfc290a9292dc6
welcomepalygame <welcomeplaygame@gmail.com>
2024-09-28 11:36:58 +0300

    $(node -p 'new Date().toISOString().slice(0, 10)')_v2

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,6 +1,11 @@
 import { findAllArticle as get } from '@/lib/action';
 import { NextResponse } from 'next/server';
+
 export const GET = async (req: Request) => {
-  const news = await get(1, 10);
+  const { searchParams } = new URL(req.url);
+  const page = Number(searchParams.get('page')) || 1;
+  const limit = 10;
+
+  const news = await get(page, limit);
   return NextResponse.json(news);
 };
