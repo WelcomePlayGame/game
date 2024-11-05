8052b76a77073bd68e1c9508f11325c730f2de4e
welcomepalygame <welcomeplaygame@gmail.com>
2024-11-05 12:52:42 +0200

    $(node -p 'new Date().toISOString().slice(0, 10)')_v2

--- app/developers/[slug]/page.tsx
+++ app/developers/[slug]/page.tsx
@@ -5,6 +5,8 @@ import { findDeveloperBySlug } from '@/lib/action';
 import Image from 'next/image';
 import classes from '@/components/games/page-game.module.css';
 import SliderGame from '@/components/slider_for_game/page-game-slider';
+import Head from 'next/head';
+
 export const generateMetadata = async ({ params }: any) => {
   const decodedSlug = decodeURIComponent(params.slug);
   const developer = await findDeveloperBySlug(decodedSlug);
@@ -14,41 +16,83 @@ export const generateMetadata = async ({ params }: any) => {
     description: developer?.seo_content,
   };
 };
+
 const Developer = async ({ params }: { params: { slug: string } }) => {
   const decodedSlug = decodeURIComponent(params.slug);
   const developer = await findDeveloperBySlug(decodedSlug);
 
   const createMarkup = (html: any) => ({ __html: html });
+
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: developer?.title ?? '',
+    logo: `${process.env.URL_AWS}${developer?.url_image}`,
+    description: developer?.seo_content ?? '',
+    url: `https://gameforyou.online/developers/${params.slug}`,
+  };
+
   return (
     <>
+      <Head>
+        {/* Schema.org structured data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:type" content="profile" />
+        <meta property="og:title" content={developer?.seo_title ?? ''} />
+        <meta
+          property="og:description"
+          content={developer?.seo_content ?? ''}
+        />
+        <meta
+          property="og:image"
+          content={`${process.env.URL_AWS}${developer?.url_image ?? ''}`}
+        />
+        <meta
+          property="og:url"
+          content={`https://gameforyou.online/developers/${params.slug}`}
+        />
+        <meta property="og:site_name" content="GameForYouOnline" />
+
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={developer?.seo_title ?? ''} />
+        <meta
+          name="twitter:description"
+          content={developer?.seo_content ?? ''}
+        />
+        <meta
+          name="twitter:image"
+          content={`${process.env.URL_AWS}${developer?.url_image ?? ''}`}
+        />
+        <meta name="twitter:site" content="@GameForYouOnline" />
+      </Head>
       <PageHeader />
       <PageNave />
-      <section className={`flex flex-col  mt-[70px]`}>
-        <div
-          className={`relative object-contain w-[300px] h-[150px] block mx-auto md:w-[600px] md:h-[300px]`}
-        >
+      <section className="flex flex-col mt-[70px]">
+        <div className="relative object-contain w-[300px] h-[150px] block mx-auto md:w-[600px] md:h-[300px]">
           {developer?.url_image && (
             <Image
               src={`${process.env.URL_AWS}${developer?.url_image}`}
               fill
               alt={developer.title}
-              className={`rounded-[5px]`}
+              className="rounded-[5px]"
             />
           )}
         </div>
-        <div
-          className={`border-b w-[300px] lg:w-[700px] block mx-auto pb-[15px]`}
-        >
-          <h1 className={` font-bold text-center mt-[30px]`}>
+        <div className="border-b w-[300px] lg:w-[700px] block mx-auto pb-[15px]">
+          <h1 className="font-bold text-center mt-[30px]">
             {developer?.title}
           </h1>
         </div>
-        <div
-          className={`font-normal  pl-[30px] pr-[30px] lg:pt-[70px] lg:pl-[250px] lg:pr-[250px]`}
-        >
-          <article
+        <div className="font-normal pl-[30px] pr-[30px] lg:pt-[70px] lg:pl-[250px] lg:pr-[250px]">
+          <section
             dangerouslySetInnerHTML={createMarkup(developer?.content)}
-            className={`${classes.content_img}`}
+            className={classes.content_img}
           />
         </div>
       </section>
--- app/platforms/[slug]/page.tsx
+++ app/platforms/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/footer/page-footer';
 import { findPlatformBySlug } from '@/lib/action';
 import Image from 'next/image';
 import classes from '@/components/games/page-game.module.css';
+import Head from 'next/head';
 
 export const generateMetadata = async ({ params }: any) => {
   const decodedSlug = decodeURIComponent(params.slug);
@@ -14,46 +15,83 @@ export const generateMetadata = async ({ params }: any) => {
     description: platform?.seo_content,
   };
 };
+
 const Platform = async ({ params }: { params: { slug: string } }) => {
   const decodedSlug = decodeURIComponent(params.slug);
   const platform = await findPlatformBySlug(decodedSlug);
 
   const createMarkup = (html: any) => ({ __html: html });
+
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'CreativeWorkSeries',
+    name: platform?.title ?? '',
+    image: `${process.env.URL_AWS}${platform?.url_image ?? ''}`,
+    description: platform?.seo_content ?? '',
+    url: `https://gameforyou.online/platforms/${params.slug}`,
+  };
+
   return (
-    <main>
+    <section>
+      <Head>
+        {/* Schema.org structured data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={platform?.seo_title ?? ''} />
+        <meta property="og:description" content={platform?.seo_content ?? ''} />
+        <meta
+          property="og:image"
+          content={`${process.env.URL_AWS}${platform?.url_image ?? ''}`}
+        />
+        <meta
+          property="og:url"
+          content={`https://gameforyou.online/platforms/${params.slug}`}
+        />
+        <meta property="og:site_name" content="GameForYouOnline" />
+
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={platform?.seo_title ?? ''} />
+        <meta
+          name="twitter:description"
+          content={platform?.seo_content ?? ''}
+        />
+        <meta
+          name="twitter:image"
+          content={`${process.env.URL_AWS}${platform?.url_image ?? ''}`}
+        />
+        <meta name="twitter:site" content="@GameForYouOnline" />
+      </Head>
       <PageHeader />
       <PageNave />
-      <section className={`flex flex-col  mt-[70px]`}>
-        <div
-          className={`relative object-contain w-[300px] h-[150px] block mx-auto md:w-[600px] md:h-[300px]`}
-        >
+      <section className="flex flex-col mt-[70px]">
+        <div className="relative object-contain w-[300px] h-[150px] block mx-auto md:w-[600px] md:h-[300px]">
           {platform?.url_image && (
             <Image
               src={`${process.env.URL_AWS}${platform?.url_image}`}
               fill
               alt={platform.title}
-              className={`rounded-[5px]`}
+              className="rounded-[5px]"
             />
           )}
         </div>
-        <div
-          className={`border-b w-[300px] lg:w-[700px] block mx-auto pb-[15px]`}
-        >
-          <h1 className={` font-bold text-center mt-[30px]`}>
-            {platform?.title}
-          </h1>
+        <div className="border-b w-[300px] lg:w-[700px] block mx-auto pb-[15px]">
+          <h1 className="font-bold text-center mt-[30px]">{platform?.title}</h1>
         </div>
-        <div
-          className={`font-normal  pl-[30px] pr-[30px] lg:pt-[70px] lg:pl-[250px] lg:pr-[250px]`}
-        >
-          <div
+        <div className="font-normal pl-[30px] pr-[30px] lg:pt-[70px] lg:pl-[250px] lg:pr-[250px]">
+          <section
             dangerouslySetInnerHTML={createMarkup(platform?.content)}
-            className={`${classes.content_img}`}
+            className={classes.content_img}
           />
         </div>
       </section>
       <Footer />
-    </main>
+    </section>
   );
 };
 
